629d44c37e2b98b103a335564a26d20090ec5db4
alilsamzey <74093863+alilsamzey@users.noreply.github.com>
2020-11-07 18:39:46 +0100

    Update Firstclass.java

--- src/firstpackage/Firstclass.java
+++ src/firstpackage/Firstclass.java
@@ -5,7 +5,8 @@ public class Firstclass {
 	public static void main(String[] args) {
 	
 		
-		System.out.println("First commit from local master");
+	System.out.println("First commit from local master");
+		System.out.println("First commit from git master");
 	}
 
 }
